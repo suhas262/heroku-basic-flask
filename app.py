9570b99308d34748c7afb5101dfe51fabd1b20d3
suhas262 <suhassuresh262@gmail.com>
2018-04-28 17:16:30 -0700

    Added uploadlog

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from datetime import datetime
 app = Flask(__name__)
 
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def homepage():
 
     return "OK"
+
+
+@app.route("/uploadlog/",methods = ['GET', 'POST', 'DELETE'])
+def getPostDataUploadLog():
+    if request.method == 'POST':
+        content = request.data
+        return "OK"
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
