7626771a083db3627a348b578b0b3ebf92dfe713
suhas262 <suhassuresh262@gmail.com>
2018-05-03 13:28:10 -0700

    Modified return message

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import database as dbConn
+import json
 
 app = Flask(__name__)
 
@@ -11,10 +12,18 @@ def homepage():
 @app.route("/uploadlog/",methods = ['GET', 'POST', 'DELETE'])
 def getPostDataUploadLog():
     if request.method == 'POST':
-        content = request.json
-        print("test")
+        try:
+            content = request.json
+        except ValueError:
+            return jsonify({
+                "status":"Fail"
+            })
         dbConn.insert_to_mongo(content)
-        return "OK"
+
+        return jsonify({
+            "status":"OK",
+            "message":"successful"
+        })
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
