52ab89056c077fa6776a55299c191d554931405d
suhas262 <suhassuresh262@gmail.com>
2018-05-06 16:07:52 -0700

    CORS support

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import database as dbConn
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def homepage():
